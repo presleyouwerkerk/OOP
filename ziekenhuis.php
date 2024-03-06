cd16c9f12d1484fb6caf1480959487d3193c33c7
Presley <presley2225@gmail.com>
2024-03-06 13:24:52 +0100

    Changes

--- ziekenhuis.php
+++ ziekenhuis.php
@@ -4,7 +4,8 @@
 
 namespace Ziekenhuis;
 
-abstract class Person {
+abstract class Person 
+{
     private string $name;
     private string $eyeColor;
     private string $hairColor;
@@ -13,74 +14,127 @@ abstract class Person {
     protected string $role;
 
     public function __construct(string $name, string $eyeColor, 
-    string $hairColor, float $height, float $weight) {
+    string $hairColor, float $height, float $weight) 
+    {
         $this->name = $name;
         $this->eyeColor = $eyeColor;
         $this->hairColor = $hairColor;
         $this->height = $height;
         $this->weight = $weight;
+        $this->role = $role;
     }
 
-    public function getName(): string {
+    public function getName(): string 
+    {
         return $this->name;
+    }
+
+    public function geteyeColor(): string
+    {
+        return $this->eyeColor;
+    }
+
+    public function getHairColor(): string
+    {
+        return $this->hairColor;
+    }
+
+    public function getHeight(): string 
+    {
+        return $this->height;
+    }
+
+    public function getWeight(): string 
+    {
+        return $this->weight;
     }
 
     abstract public function setRole(string $role);
 }
 
-class Patient extends Person {
+class Patient extends Person 
+{
     private float $payment;
 
     public function __construct(string $name, string $eyeColor, 
-    string $hairColor, float $height, float $weight, float $payment) {
+    string $hairColor, float $height, float $weight, float $payment) 
+    {
         parent::__construct($name, $eyeColor, $hairColor, $height, $weight);
         $this->payment = $payment;
     }
 
-    public function setRole(string $role) {
+    public function setRole(string $role) 
+    {
         $this->role = $role;
     }
 
-    public function getPayment() {
+    public function getPayment() 
+    {
         return $this->payment;
     }
 }
 
-abstract class Staff extends Person {
+abstract class Staff extends Person 
+{
     protected float $salary;
     abstract public function setSalary(float $amount);
     abstract public function getSalary(): float;
 }
 
-class Doctor extends Staff {
-    public function setSalary(float $amount) {
+class Doctor extends Staff 
+{
+    public function setSalary(float $amount) 
+    {
         $this->salary = $amount;
     }
 
-    public function getSalary(): float {
+    public function getSalary(): float 
+    {
         return $this->salary; 
     }
 
-    public function setRole(string $role) {
+    public function setRole(string $role) 
+    {
         $this->role = $role;
+    }
+
+    public function getDoctorSalary(): float 
+    {
+        $appointment = new Appointment;
+        $costs = $this->getCosts();
+        $totalCosts = $costs[0];
+        return $totalCosts;
     }
 }
 
-class Nurse extends Staff {
-    public function setSalary(float $amount) {
+class Nurse extends Staff 
+{
+    public function setSalary(float $amount) 
+    {
         $this->salary = $amount;
     }
 
-    public function getSalary(): float {
-        return $this->salary;
+    public function getSalary(): float 
+    {
+        return $this->salary; 
     }
 
-    public function setRole(string $role) {
+    public function setRole(string $role) 
+    {
         $this->role = $role;
+    }
+
+    public function getNurseSalary(): float 
+    {
+        $appointment = new Appointment;
+        $costs = $this->getCosts();
+        $totalCosts = $costs[1];
+        return $totalCosts;
     }
 }
 
-class Appointment {
+class Appointment 
+{
     private Patient $patient;
     private Doctor $doctor;
     private array $nurses;
@@ -90,52 +144,59 @@ class Appointment {
     private static array $appointments = [];
 
     public function setAppointment(Patient $patient, Doctor $doctor, 
-    array $nurses, \DateTime $beginTime, \DateTime $endTime) {
+    array $nurses, \DateTime $beginTime, \DateTime $endTime) 
+    {
         $this->patient = $patient;
         $this->doctor = $doctor;
         $this->nurses = $nurses;
         $this->beginTime = $beginTime;
         $this->endTime = $endTime;
+        self::$appointments[] = $appointment;
         self::$count++;
     }
 
-    public function addNurse(Nurse $nurse) {
+    public function addNurse(Nurse $nurse) 
+    {
         $this->nurses[] = $nurse;
     }
 
-    public function getDoctor(): Doctor {
+    public function getDoctor(): Doctor 
+    {
         return $this->doctor;
     }
 
-    public function getPatient(): Patient {
-        return $this->patient;
-    }
-    
-    public function getNurses(): array {
+    public function getNurses(): array 
+    {
         return $this->nurses;
     }
 
-    public function getBeginTime(): string {
+    public function getPatient(): Patient 
+    {
+        return $this->patient;
+    }
+
+    public function getBeginTime(): string 
+    {
         return $this->beginTime->format('Y-m-d H:i:s');
     }
 
-    public function getEndTime(): string {
+    public function getEndTime(): string 
+    {
         return $this->endTime->format('Y-m-d H:i:s');
     }
 
-    public static function getCount(): int {
+    public static function getCount(): int 
+    {
         return self::$count;
     }
 
-    public static function addAppointment(Appointment $appointment) {
-        self::$appointments[] = $appointment;
-    }
-
-    public static function getAppointments(): array {
+    public static function getAppointments(): array 
+    {
         return self::$appointments;
     }
 
-    public function getTimeDifference(): float {
+    public function getTimeDifference(): float 
+    {
         $beginTimestamp = $this->beginTime->getTimestamp();
         $endTimestamp = $this->endTime->getTimestamp();
     
@@ -146,17 +207,19 @@ class Appointment {
         return $hours;
     }
 
-    public function getCosts(): float {
+    public function getCosts(): float 
+    {
         $duration = $this->getTimeDifference();
 
         $doctorCost = $duration * $this->doctor->getSalary();
 
         $totalNurseBonus = 0;
 
-        foreach ($this->nurses as $nurse) {
+        foreach ($this->nurses as $nurse) 
+        {
             $totalNurseBonus += $duration * $nurse->getSalary();
         }
 
-        return $doctorCost + $totalNurseBonus;
+        return [$doctorCost, $totalNurseBonus];
     }
 }
