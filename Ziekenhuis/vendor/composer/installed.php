036c4f1702c7c99daf241c173cdfb7a8544a1ba1
Presley <presley2225@gmail.com>
2024-03-13 12:38:58 +0100

    forgot vendor file

---
+++ Ziekenhuis/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'presley/hospital',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cf216868b33579cb96649a641c9b6efb5a36a7ed',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'presley/hospital' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cf216868b33579cb96649a641c9b6efb5a36a7ed',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
